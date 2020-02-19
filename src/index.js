2c2e287cfb1d8e23be1d23b89d5f6232bef1940f
Anna <ana-karp@users.noreply.github.com>
2020-02-19 19:24:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (!array || array.length === 0) ? 0 : Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    return (!array || array.length === 0) ? 0 : Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || array.length === 0) {
+        return 0;
+    }
+    let sum = array.reduce((a, b) => a + b);
+    return sum / array.length;
 }
